7b0bd58c359f5ea21af907cb90234171a6cfca5c
Markus Amshove <scm@amshove.org>
2016-06-20 21:31:31 +0200

    Use the correct chesefolder objects

--- photobox/photobox.py
+++ photobox/photobox.py
@@ -1,3 +1,4 @@
+from cheesefolder import Cheesefolder
 from photofolder import Photofolder
 from folder import RealFolder
 from gphotocamera import Gphoto
@@ -8,14 +9,15 @@ from rcswitch import RCSwitch
 # config #
 ##########
 photodirectory = '/var/www/html/'
-cheesepicfolder = '/home/pi/cheesepics/'
+cheesepicpath = '/home/pi/cheesepics/'
 windowwidth = 1024
 windowheight = 768
 camera = Gphoto()
 switch = RCSwitch(2352753, 2352754, "NOT_IMPLEMENTED")
 ##########
 filesystemFolder = RealFolder(photodirectory)
-cheesepicFolder = RealFolder(cheesepicfolder)
+cheesepicFolder = RealFolder(cheesepicpath)
+cheesef = Cheesefolder(cheesepicFolder)
 photofolder = Photofolder(filesystemFolder)
-photobox = Photobox((windowwidth, windowheight), photofolder, camera, switch)
+photobox = Photobox((windowwidth, windowheight), photofolder, camera, switch, cheesef)
 photobox.start()
